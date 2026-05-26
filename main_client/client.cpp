4d0638ba50a888598b8674d5e1a7d0fc2c7a3dfb
PadureanTudor <tudor.padurean06@e-uvt.ro>
2026-05-26 15:51:37 +0300

    Migrare la smart pointers

--- main_client/client.cpp
+++ main_client/client.cpp
@@ -1,7 +1,49 @@
+#include <cstring>
 #include <iostream>
+#include <memory>
 
 #include "../src_clase/Candidat.h"
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
+void todo() {
+    std::cerr << "Functionalitate neimplementata";
+    exit(1);
+}
+
+void eroare(const char * str) {
+    std::cerr << str;
+    exit(1);
+}
+
+void parse_cli(int argc, char* argv[]) {
+    switch (argc) {
+        case 1:
+            if (strcmp(argv[1], "vizualizare_joburi") == 0) {
+                return todo();
+            }
+            break;
+        case 2:
+            if (strcmp(argv[1], "vizualizare_joburi_filtrat") == 0) {
+                return todo();
+            } if (strcmp(argv[1], "vizualizare_aplicatii_proprii") == 0) {
+                return todo();
+            }
+            break;
+        case 3:
+            if (strcmp(argv[1], "retragere_aplicatie") == 0) {
+                return todo();
+            }
+            break;
+        case 4:
+            if (strcmp(argv[1], "aplicare") == 0) {
+                return todo();
+            }
+            break;
+        default:
+            break;
+    }
+    return eroare("Argumente invalide");
+}
+
+int main(int argc, char* argv[]) {
+    parse_cli(argc, argv);
 }
--- src_clase/Candidat.h
+++ src_clase/Candidat.h
@@ -5,6 +5,7 @@
 #ifndef PROIECT_C_CANDIDAT_H
 #define PROIECT_C_CANDIDAT_H
 
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -12,19 +13,19 @@
 
 class Candidat {
     std::string nume_candidat;
-    std::vector<Aplicatie*> aplicatii;
+    std::vector<std::shared_ptr<Aplicatie>> aplicatii;
 public:
     Candidat(std::string nume_candidat);
-    ~Candidat();
+    ~Candidat() = default;
 
     std::string get_nume_candidat();
 
     // Returns a reference to the candidate's applications for read-only purposes.
-    std::vector<Aplicatie*>* get_aplicatii();
+    std::vector<std::shared_ptr<Aplicatie>> get_aplicatii();
 
     // Returns empty string ("") if the operation was successful, otherwise returns an error message
-    std::string adauga_aplicatie(Aplicatie aplicatie);
-    std::string retragere_aplicatie(const Aplicatie* aplicatie);
+    std::string adauga_aplicatie(const std::shared_ptr<Aplicatie>& aplicatie);
+    std::string retragere_aplicatie(std::string titlu_job);
 
     // Reads from aplicatii.txt and initializes a list of candidates with their respective applications
     static std::vector<Candidat*> initializare_candidati();
